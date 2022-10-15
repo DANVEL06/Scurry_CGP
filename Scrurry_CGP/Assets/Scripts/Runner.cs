2a9ce888b17c292ddc294680d049e45be2e7fb0e
Spyktail <andrewrodgers326@yahoo.com>
2022-10-15 06:03:46 -0400

    ScurryButton_v1.0
    got everything working except the cooldown timer between uses

--- Scrurry_CGP/Assets/Scripts/Runner.cs
+++ Scrurry_CGP/Assets/Scripts/Runner.cs
@@ -1,16 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Runner : MonoBehaviour
 {
-public int movespeed = 10;
-public Vector3 userDirection = Vector3.forward;
-public int scurryspeed = 30;
+    public int movespeed = 10;
+    public int normalSpeed = 10;
+    public int scurryspeed = 50;
+    public int fastSpeed = 50;
+    public Vector3 userDirection = Vector3.forward;
+
+    public float speedBoostDuration = 2.0f;
+
+    public Button ScurryButton;
+    public float scurryButtonCooldown = 5.0f;
+    private bool goingFast = false;
+
+  
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ScurryButton.onClick.AddListener(SpeedChange);
     }
 
     // Update is called once per frame
@@ -24,10 +37,50 @@ public int scurryspeed = 30;
         if (Input.GetKey(KeyCode.D))  
         {  
             transform.Translate(-0.1f, 0f, 0f);  
-        }  
-        if (Input.GetKey(KeyCode.F))
+        }
+      
+        if (goingFast)
         {
-            movespeed = scurryspeed;
-        } 
+            if (speedBoostDuration > 0)
+            {
+                speedBoostDuration -= Time.deltaTime;
+                if (speedBoostDuration <= 0.01f)
+                {
+                    movespeed = normalSpeed;
+                    Debug.Log("so slow again");
+                    goingFast = false;
+                    speedBoostDuration = 2.0f;
+                    
+                
+                }
+            }
+        }
+
+        /*if (scurryButtonCooldown > 0)
+        {
+            scurryspeed = normalSpeed;
+            scurryButtonCooldown -= Time.deltaTime;
+            Debug.Log("can't run");
+
+        }
+        if (scurryButtonCooldown <= 0.01f)
+        {
+            scurryspeed = fastSpeed;
+            Debug.Log("speedy once more");
+            scurryButtonCooldown = 5.0f;
+        }
+
+        if (movespeed > 11)
+        {
+            
+        }*/
+    }
+
+    void SpeedChange()
+    {
+        Debug.Log("is working");
+        movespeed = scurryspeed;
+        Debug.Log("gotta go fast");
+        goingFast = true;
     }
 }
