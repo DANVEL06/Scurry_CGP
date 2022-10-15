03f3158bde58bc4eebf791bade2d7d68a98a8b22
DANVEL06 <danvelasco20@hotmail.com>
2022-10-15 13:19:47 -0400

    added touch movement

---
+++ Scrurry_CGP/Assets/Scripts/TouchMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchMovement : MonoBehaviour
+{
+    private Touch touch;
+    private float speedModifier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speedModifier = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            if(touch.phase == TouchPhase.Moved)
+            {
+                transform.position = new Vector3(
+                   transform.position.x + touch.deltaPosition.x * speedModifier,
+                   transform.position.y,
+                   transform.position.z + touch.deltaPosition.y * speedModifier);
+                
+            }
+        }
+    }
+}
