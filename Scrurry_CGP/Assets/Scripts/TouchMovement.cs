9019d46577616137f94956a830a95b38d44c751a
jabeleda1 <jabeleda2@knights.ucf.edu>
2022-11-06 13:02:53 -0500

    Progress on bedroom level and new movement script
    added more clutter to bedroom and changed movement script

--- Scrurry_CGP/Assets/Scripts/TouchMovement.cs
+++ Scrurry_CGP/Assets/Scripts/TouchMovement.cs
@@ -7,6 +7,7 @@ public class TouchMovement : MonoBehaviour
     private Touch touch;
     private float speedModifier;
     private int dontMove = 0;
+    public Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
@@ -22,11 +23,16 @@ public class TouchMovement : MonoBehaviour
             touch = Input.GetTouch(0);
             if(touch.phase == TouchPhase.Moved)
             {
+                rb.velocity = new Vector3(
+                    rb.velocity.x + touch.deltaPosition.x * speedModifier,
+                    rb.velocity.y + touch.deltaPosition.y * dontMove,
+                    rb.velocity.z + touch.deltaPosition.y * dontMove);
+             
                 
-                transform.position = new Vector3(
-                   transform.position.x + touch.deltaPosition.x * speedModifier,
-                   transform.position.y + touch.deltaPosition.y * dontMove,
-                   transform.position.z + touch.deltaPosition.y * dontMove);
+                 // transform.position = new Vector3(
+                  // transform.position.x + touch.deltaPosition.x * speedModifier,
+                  // transform.position.y + touch.deltaPosition.y * dontMove,
+                  // transform.position.z + touch.deltaPosition.y * dontMove);
                 
             }
         }
