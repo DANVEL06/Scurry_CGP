e611cbaad7401b0fff8e1a064a76750960dc181d
jabeleda1 <jabeleda2@knights.ucf.edu>
2022-11-30 15:05:17 -0500

    Unlock level success
    you can unlock levels successfully

--- Scrurry_CGP/Assets/Scripts/LevelUnlockHandler.cs
+++ Scrurry_CGP/Assets/Scripts/LevelUnlockHandler.cs
@@ -5,21 +5,32 @@ using UnityEngine.UI;
 
 public class LevelUnlockHandler : MonoBehaviour
 {
+    //all them buttons
+    public GameObject[] buttonArray;
+    public GameObject[] buttonIMAGEarray;
+
+    public int buttonNUM;
+
     [SerializeField] Button[] buttons;
     int unlockedLevelsNumber;
     
     // Start is called before the first frame update
     void Start()
     {
+        //make button array
+
         if (!PlayerPrefs.HasKey("levelsUnlocked")) {
             PlayerPrefs.SetInt("levelsUnlocked", 1);
         }
 
         unlockedLevelsNumber = PlayerPrefs.GetInt("levelsUnlocked");
 
-        for (int i=0; i< buttons.Length; i++) {
+        for (int i=0; i< buttonNUM; i++) {
 
-            buttons[i].interactable = false;
+            buttonIMAGEarray[i].SetActive(true);
+            buttonArray[i].SetActive(false);
+            //buttons[i].interactable = false;
+            //change buttons to images
         }
     }
 
@@ -29,8 +40,11 @@ public class LevelUnlockHandler : MonoBehaviour
         unlockedLevelsNumber = PlayerPrefs.GetInt("levelsUnlocked");
 
         for (int i=0; i<unlockedLevelsNumber; i++) {
-
-            buttons[i].interactable = true;
+            buttonArray[i].SetActive(true);
+            buttonIMAGEarray[i].SetActive(false);
+            //buttons[i].interactable = true;
         }
+
+        Debug.Log("levelsUnlocked: "+unlockedLevelsNumber);
     }
 }
