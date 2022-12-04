412db3f6594ded0a936610ea7507f1a5fe646038
Zeldronic <graciana.baker@knights.ucf.edu>
2022-12-03 19:15:13 -0500

    main menu level locking system

--- Scrurry_CGP/Assets/Scripts/LevelUnlockHandler.cs
+++ Scrurry_CGP/Assets/Scripts/LevelUnlockHandler.cs
@@ -5,32 +5,52 @@ using UnityEngine.UI;
 
 public class LevelUnlockHandler : MonoBehaviour
 {
+    //all them buttons
+    public GameObject[] buttonArray;
+    public GameObject[] buttonIMAGEarray;
+
+    public int buttonNUM;
+
     [SerializeField] Button[] buttons;
     int unlockedLevelsNumber;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        if (!PlayerPrefs.HasKey("levelsUnlocked")) {
+        //make button array
+
+        if (!PlayerPrefs.HasKey("levelsUnlocked"))
+        {
             PlayerPrefs.SetInt("levelsUnlocked", 1);
         }
 
         unlockedLevelsNumber = PlayerPrefs.GetInt("levelsUnlocked");
 
-        for (int i=0; i< buttons.Length; i++) {
+      //  for (int i = 0; i < buttons.Length; i++)
+       // {
+            for (int i = 0; i < buttonNUM; i++)
+            {
 
-            buttons[i].interactable = false;
-        }
-    }
+                buttons[i].interactable = false;
+                buttonIMAGEarray[i].SetActive(true);
+                buttonArray[i].SetActive(false);
+                //buttons[i].interactable = false;
+                //change buttons to images
+            }
+      //  }
 
-    // Update is called once per frame
-    void Update()
-    {
-        unlockedLevelsNumber = PlayerPrefs.GetInt("levelsUnlocked");
+
+ 
+         unlockedLevelsNumber = PlayerPrefs.GetInt("levelsUnlocked");
 
         for (int i=0; i<unlockedLevelsNumber; i++) {
 
             buttons[i].interactable = true;
+            buttonArray[i].SetActive(true);
+            buttonIMAGEarray[i].SetActive(false);
+             //buttons[i].interactable = true;
         }
+
+        Debug.Log("levelsUnlocked: " + unlockedLevelsNumber);
     }
 }
